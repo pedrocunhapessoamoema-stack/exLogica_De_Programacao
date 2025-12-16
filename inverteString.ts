77f8f087de022e0764c9579dbbf854c4b4a9192e
Pedro <pedrocunha.pessoamoema@gmail.com>
2025-12-16 19:34:54 -0300

    Organização

--- inverteString.ts
+++
@@ -1,9 +0,0 @@
-function inverteString (palavra:string):string{
-  return palavra.split("").reverse().join("");
-}
-
-
-//testes
-console.log(inverteString("hello"));
-console.log(inverteString("abacaxi"))
-console.log(inverteString("12345"));
--- numerosPositivos.ts
+++
@@ -1,9 +0,0 @@
-function verPositivo(numeros:number[]){
-  let positivos: number [] = []
-  for (let i = 0; i <= numeros.length; i++) {
-    if (numeros[i] >= 0) positivos.push(numeros[i]);
-  }
-  return positivos;
-}
-
-console.log(verPositivo([5, 2, 4, 6, -5, -9]));
